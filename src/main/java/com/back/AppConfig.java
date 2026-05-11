a7443d066b3615ac826a831cdccedd42e088a2ce
oxcm07 <oxcm0727@gmail.com>
2026-05-12 00:25:01 +0900

    008

--- src/main/java/com/back/AppConfig.java
+++ src/main/java/com/back/AppConfig.java
@@ -1,17 +1,26 @@
 package com.back;
 
+import org.springframework.boot.ApplicationRunner;
 import org.springframework.context.annotation.Bean;
 import org.springframework.context.annotation.Configuration;
+import org.springframework.core.annotation.Order;
 
 @Configuration
 public class AppConfig {
     @Bean
-    PersonRepository personRepository() {
-        return new PersonRepository(1);
+    int version() {
+        return 55;
     }
 
     @Bean
-    PersonRepository personRepositoryV2() {
-        return new PersonRepository(2);
+    @Order(2)
+    public ApplicationRunner myApplicationRunner() {
+        return new MyApplicationRunner();
+    }
+
+    @Bean
+    @Order(1)
+    public ApplicationRunner myApplicationRunner2() {
+        return new MyApplicationRunner2();
     }
 }
